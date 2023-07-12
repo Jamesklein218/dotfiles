05b58febd3a3b710729648ddf76339f9196825ce
Tran Ngoc Dang Khoa <tndangkhoa218@gmail.com>
2023-07-12 16:01:45 +0700

    change

--- nvim/lua/khoa/core/options.lua
+++ nvim/lua/khoa/core/options.lua
@@ -6,10 +6,10 @@ opt.relativenumber = true
 
 -- tabs & indentation
 opt.expandtab = true
-opt.shiftwidth = 2
+opt.shiftwidth = 4
 opt.smartindent = true
-opt.tabstop = 2
-opt.softtabstop = 2
+opt.tabstop = 4
+opt.softtabstop = 4
 
 -- wrapping
 opt.wrap = false
