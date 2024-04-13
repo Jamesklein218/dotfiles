8d8750860d7f6ee098bb8fb29663f0de9b8bc7ef
Tran Ngoc Dnag Khoa <tndangkhoa218@gmail.com>
2024-04-13 10:51:36 +0700

    tmux config and more

--- nvim/lua/khoa/core/options.lua
+++ nvim/lua/khoa/core/options.lua
@@ -6,10 +6,10 @@ opt.relativenumber = true
 
 -- tabs & indentation
 opt.expandtab = true
-opt.shiftwidth = 4
+opt.shiftwidth = 2
 opt.smartindent = true
-opt.tabstop = 4
-opt.softtabstop = 4
+opt.tabstop = 2
+opt.softtabstop = 2
 
 -- wrapping
 opt.wrap = false
