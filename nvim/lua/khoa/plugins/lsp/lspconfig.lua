180dfc70361b1f1cafde7d0a7eca13435082e560
Tran Ngoc Dnag Khoa <tndangkhoa218@gmail.com>
2026-09-02 10:24:52 +0700

    fix: syntax

--- nvim/lua/khoa/plugins/lsp/lspconfig.lua
+++ nvim/lua/khoa/plugins/lsp/lspconfig.lua
@@ -1,161 +1,12 @@
 -- import lspconfig plugin safely
-local lspconfig_status, lspconfig = pcall(require, "lspconfig")
-if not lspconfig_status then
-	return
-end
-
--- import cmp-nvim-lsp plugin safely
-local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-if not cmp_nvim_lsp_status then
-	return
-end
-
-local keymap = vim.keymap -- for conciseness
-
--- enable keybinds only for when lsp server available
-local on_attach = function(client, bufnr)
-	-- keybind options
-	local opts = { noremap = true, silent = true, buffer = bufnr }
-
-	-- set keybinds
-	keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts) -- show definition, references
-	keymap.set("n", "gD", "<Cmd>Lspsaga goto_definition<CR>", opts) -- got to definition
-	keymap.set("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts) -- see definition and make edits in window
-	keymap.set("n", "gL", "<cmd>Lspsaga show_line_diagnostics<CR>", opts) -- show line error msg
-	keymap.set("n", "gC", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts) -- show cursor error msg
-	keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts) -- see available code actions
-	keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts) -- smart rename
-	keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts) -- show documentation for what is under cursor
-	keymap.set("n", "<A-d>", "<cmd>Lspsaga term_toggle<CR>", opts) -- toggle terminal
-end
-
--- used to enable autocompletion (assign to every lsp server config)
-local capabilities = cmp_nvim_lsp.default_capabilities()
-
--- Change the Diagnostic symbols in the sign column (gutter)
--- (not in youtube nvim video)
-local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
-for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-end
-
--- configure html server
-lspconfig["html"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure typescript server with plugin
-lspconfig["ts_ls"].setup({
-	server = {
-		capabilities = capabilities,
-		on_attach = on_attach,
-	},
-})
-
--- configure css server
-lspconfig["cssls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure tailwindcss server
-lspconfig["tailwindcss"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-})
-
--- configure emmet language server
-lspconfig["emmet_ls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	filetypes = { "html", "typescriptreact", "javascriptreact", "css", "sass", "scss", "less", "svelte" },
-})
-
--- configure lua server (with special settings)
-lspconfig["lua_ls"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	settings = { -- custom settings for lua
-		Lua = {
-			-- make the language server recognize "vim" global
-			diagnostics = {
-				globals = { "vim" },
-			},
-			workspace = {
-				-- make language server aware of runtime files
-				library = {
-					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-					[vim.fn.stdpath("config") .. "/lua"] = true,
-				},
-			},
-		},
-	},
-})
-
--- configure C/C++ server
-lspconfig["clangd"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	cmd = {
-		"clangd",
-		"--offset-encoding=utf-16",
-		"--query-driver=/usr/local/include/", -- For #include <bits/stdc++.h>
-	},
-	-- root_dir = lspconfig.util.root_pattern(
-	-- 	".clangd",
-	-- 	".clang-tidy",
-	-- 	".clang-format",
-	-- 	"compile_commands.json",
-	-- 	"compile_flags.txt",
-	-- 	"configure.ac",
-	-- 	".git"
-	-- ),
-})
-
--- configure Golang server
-lspconfig["gopls"].setup({
-	cmd = { "gopls" },
-	filetypes = { "go", "gomod", "gowork", "gotmpl" },
-	root_dit = lspconfig.util.root_pattern("go.work", "go.mod", ".git"),
-	single_file_support = true,
-})
-
--- configure Python Server
-lspconfig["pyright"].setup({
-	on_attach = on_attach,
-	capabilities = capabilities,
-	filetypes = { "python" },
-	settings = {
-		python = {
-			analysis = {
-				autoSearchPaths = true,
-				diagnosticMode = "workspace",
-				useLibraryCodeForTypes = true,
-			},
-		},
-	},
-	single_file_support = true,
-})
-
--- configure Docker and Docker Compose
-lspconfig["dockerls"].setup({})
-lspconfig["docker_compose_language_service"].setup({})
-
-lspconfig["drools_lsp"].setup({})
-vim.cmd([[ autocmd BufRead,BufNewFile *.drl set filetype=drools ]])
-
-lspconfig["zls"].setup({
-	on_attach = on_attach,
 local ok, lspconfig = pcall(require, "lspconfig")
 if not ok then
-    return
+	return
 end
 
 local ok_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not ok_cmp then
-    return
+	return
 end
 
 local util = require("lspconfig.util")
@@ -167,19 +18,19 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 --------------------------------------------------------------------------------
 
 local signs = {
-    Error = " ",
-    Warn = " ",
-    Hint = "󰠠 ",
-    Info = " ",
+	Error = " ",
+	Warn = " ",
+	Hint = "󰠠 ",
+	Info = " ",
 }
 
 for type, icon in pairs(signs) do
-    local hl = "DiagnosticSign" .. type
-    vim.fn.sign_define(hl, {
-        text = icon,
-        texthl = hl,
-        numhl = "",
-    })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, {
+		text = icon,
+		texthl = hl,
+		numhl = "",
+	})
 end
 
 --------------------------------------------------------------------------------
@@ -187,25 +38,25 @@ end
 --------------------------------------------------------------------------------
 
 local on_attach = function(_, bufnr)
-    local opts = {
-        noremap = true,
-        silent = true,
-        buffer = bufnr,
-    }
+	local opts = {
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	}
 
-    local map = vim.keymap.set
+	local map = vim.keymap.set
 
-    map("n", "gf", "<cmd>Lspsaga finder<CR>", opts)
-    map("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
-    map("n", "gD", "<cmd>Lspsaga goto_definition<CR>", opts)
+	map("n", "gf", "<cmd>Lspsaga finder<CR>", opts)
+	map("n", "gd", "<cmd>Lspsaga peek_definition<CR>", opts)
+	map("n", "gD", "<cmd>Lspsaga goto_definition<CR>", opts)
 
-    map("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
+	map("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
 
-    map("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
-    map("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
+	map("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
+	map("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
 
-    map("n", "gL", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-    map("n", "gC", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
+	map("n", "gL", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
+	map("n", "gC", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
 end
 
 --------------------------------------------------------------------------------
@@ -213,8 +64,8 @@ end
 --------------------------------------------------------------------------------
 
 local default = {
-    capabilities = capabilities,
-    on_attach = on_attach,
+	capabilities = capabilities,
+	on_attach = on_attach,
 }
 
 --------------------------------------------------------------------------------
@@ -222,19 +73,19 @@ local default = {
 --------------------------------------------------------------------------------
 
 local servers = {
-    "html",
-    "cssls",
-    "tailwindcss",
-    "dockerls",
-    "docker_compose_language_service",
-    "drools_lsp",
-    "zls",
+	"html",
+	"cssls",
+	"tailwindcss",
+	"dockerls",
+	"docker_compose_language_service",
+	"drools_lsp",
+	"zls",
 }
 
 for _, server in ipairs(servers) do
-    if lspconfig[server] then
-        lspconfig[server].setup(default)
-    end
+	if lspconfig[server] then
+		lspconfig[server].setup(default)
+	end
 end
 
 --------------------------------------------------------------------------------
@@ -242,9 +93,9 @@ end
 --------------------------------------------------------------------------------
 
 if lspconfig.ts_ls then
-    lspconfig.ts_ls.setup(default)
+	lspconfig.ts_ls.setup(default)
 elseif lspconfig.tsserver then
-    lspconfig.tsserver.setup(default)
+	lspconfig.tsserver.setup(default)
 end
 
 --------------------------------------------------------------------------------
@@ -252,18 +103,18 @@ end
 --------------------------------------------------------------------------------
 
 lspconfig.emmet_ls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    filetypes = {
-        "html",
-        "css",
-        "scss",
-        "sass",
-        "less",
-        "javascriptreact",
-        "typescriptreact",
-        "svelte",
-    },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	filetypes = {
+		"html",
+		"css",
+		"scss",
+		"sass",
+		"less",
+		"javascriptreact",
+		"typescriptreact",
+		"svelte",
+	},
 })
 
 --------------------------------------------------------------------------------
@@ -271,19 +122,19 @@ lspconfig.emmet_ls.setup({
 --------------------------------------------------------------------------------
 
 lspconfig.lua_ls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true),
-                checkThirdParty = false,
-            },
-        },
-    },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		Lua = {
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+				checkThirdParty = false,
+			},
+		},
+	},
 })
 
 --------------------------------------------------------------------------------
@@ -291,21 +142,17 @@ lspconfig.lua_ls.setup({
 --------------------------------------------------------------------------------
 
 lspconfig.gopls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    cmd = { "gopls" },
-    filetypes = {
-        "go",
-        "gomod",
-        "gowork",
-        "gotmpl",
-    },
-    root_dir = util.root_pattern(
-        "go.work",
-        "go.mod",
-        ".git"
-    ),
-    single_file_support = true,
+	capabilities = capabilities,
+	on_attach = on_attach,
+	cmd = { "gopls" },
+	filetypes = {
+		"go",
+		"gomod",
+		"gowork",
+		"gotmpl",
+	},
+	root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+	single_file_support = true,
 })
 
 --------------------------------------------------------------------------------
@@ -313,17 +160,17 @@ lspconfig.gopls.setup({
 --------------------------------------------------------------------------------
 
 lspconfig.pyright.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                diagnosticMode = "workspace",
-                useLibraryCodeForTypes = true,
-            },
-        },
-    },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true,
+			},
+		},
+	},
 })
 
 --------------------------------------------------------------------------------
@@ -331,12 +178,12 @@ lspconfig.pyright.setup({
 --------------------------------------------------------------------------------
 
 lspconfig.clangd.setup({
-    capabilities = capabilities,
-    on_attach = on_attach,
-    cmd = {
-        "clangd",
-        "--offset-encoding=utf-16",
-    },
+	capabilities = capabilities,
+	on_attach = on_attach,
+	cmd = {
+		"clangd",
+		"--offset-encoding=utf-16",
+	},
 })
 
 --------------------------------------------------------------------------------
@@ -344,7 +191,6 @@ lspconfig.clangd.setup({
 --------------------------------------------------------------------------------
 
 vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
-    pattern = "*.drl",
-    command = "set filetype=drools"
+	pattern = "*.drl",
+	command = "set filetype=drools",
 })
-
